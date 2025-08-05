6f1e922a8408b6b45209afe990eff2b09fe6a046
MollyWatts <molly.watts@yale.edu>
2025-08-05 14:10:08 -0400

    quick fix for GPS time in fetch

--- worker2/fetch_data_to_h5.py
+++ worker2/fetch_data_to_h5.py
@@ -5,14 +5,14 @@ import time
 from datetime import datetime
 
 CHANNEL = 'Y1:RDS-LES_YAW_MON_OUT_DQ' # specify the channel to fetch data from
-DURATION = 60 * 60  # Fetch data for 1 hour (3600 seconds)
-USE_END_Now = True  # Use current time as end time
+DURATION = 60 * 60 * 24  # Fetch data for 1 day (86400 seconds)
+USE_END_Now = False  # Use current time as end time
 
 # Compute start and end times
 if USE_END_Now:
     gps_end = int(time.time())  # Current time as end time
 else:
-    gps_end = 1700000000 # Example fixed end time - change as needed
+    gps_end = 1437852416 # Example fixed GPS end time (Jul 30th 2025) - change as needed
 gps_start = gps_end - DURATION  # Start time is whatever duration before end time
 
 # Connect to NDS2 connection
@@ -25,13 +25,16 @@ print('Duration:', DURATION, 'seconds')
 
 # FETCH data: buffers are used to store RAW data
 # LIGO documentation: https://git.ligo.org/nds/nds2-client/-/blob/master/swig/python/module/nds_python.i?ref_type=heads
-buffers = conn.fetch([CHANNEL], gps_start, gps_end)  # Adjust start and end times as needed
+buffers = conn.fetch(gps_start, gps_end, [CHANNEL,])  # Adjust start and end times as needed
 if not buffers:
     print(f"No data found for {CHANNEL} between {gps_start} and {gps_end}.")
     exit()
 else:
     print(f"Fetching data from {CHANNEL} between {gps_start} and {gps_end}.")
 
+#buf = buffers[0]
+# print(buf)
+# print(dir(buf))
 # combine the buffers into a single array
 data = np.concatenate([buf.data for buf in buffers])
 sample_rate = buffers[0].sample_rate
@@ -39,7 +42,7 @@ sample_rate = buffers[0].sample_rate
 # timestamps for the data
 timestamps = []
 for buf in buffers:
-    t0 = buf.seconds + buf.nanoseconds * 1e-9
+    t0 = buf.gps_seconds + buf.gps_nanoseconds * 1e-9
     dt = 1.0 / buf.sample_rate
     timestamps.extend(t0 + k * dt for k in range(len(buf.data)))
     #timestamps.extend(np.arange(t0, t0 + dt * len(buf['data']), dt))
@@ -47,7 +50,7 @@ timestamps = np.array(timestamps)
 
 # Create HDF5 file
 #filename = f'yaw_data_{int(time.time())}.hdf5'
-filename = f"{CHANNEL.replace(':', '_')}_{gps_start}_{gps_end}.hdf5"
+filename = f"data/{CHANNEL.replace(':', '_')}_{gps_start}_{gps_end}.hdf5"
 with h5py.File(filename, 'w') as f:
     f.attrs['channel'] = CHANNEL
     f.attrs['gps_start'] = gps_start
